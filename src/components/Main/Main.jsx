c739461497e624c1850e52ac85db032324e8a49e
Ronee <137155067+RONHEL235@users.noreply.github.com>
2025-01-01 14:05:19 +0200

    refactor Main component to integrate context and enhance input handling; update context logic for message sending

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,12 @@
 import './Main.css'
 import { assets } from '../../assets/assets'
+import { Context } from '../../context/Context'
+import { useContext } from 'react'
 
 const Main = () => {
+
+    const {onSent, recentPrompt, showResult, loading, resultData, input, setInput} = useContext(Context)
+
   return (
     <div className="main">
         <div className="nav">
@@ -33,11 +38,14 @@ const Main = () => {
 
                 <div className="main-bottom">
                     <div className="search-box">
-                        <input type="text" placeholder="Enter a prompt here" />
+                        <input onChange={(e) => setInput(e.target.value)}
+                        value={input} 
+                        type="text" 
+                        placeholder="Enter a prompt here" />
                         <div>
                             <img src={assets.gallery_icon} alt="" />
                             <img src={assets.mic_icon} alt="" />
-                            <img src={assets.send_icon} alt="" />
+                            <img onClick={() => onSent()} src={assets.send_icon} alt="" />
                         </div>
                     </div>
                     <p className="bottom-info">
