5b3b4b5ddd686bb1bc2df3f50f5540889a1dc6dd
Ronee <137155067+RONHEL235@users.noreply.github.com>
2025-01-01 12:35:23 +0200

    add Google Generative AI integration and create ContextProvider for state management

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
+import ContextProvider from "./context/Context.jsx";
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <ContextProvider>
     <App />
-  </StrictMode>,
+  </ContextProvider>,
 )
