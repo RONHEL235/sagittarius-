d80c4cce2ca3dfaa4ad2a64527fba195d46ff786
Ronee <137155067+RONHEL235@users.noreply.github.com>
2025-01-01 18:00:05 +0200

    add loading animation and enhance result rendering in Main component; implement delayed text display for improved user experience

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -12,13 +12,40 @@ const ContextPovider = (props) => {
     const [loading, setLoading] = useState(false);
     const [resultData, setResultData] = useState([]);
 
+    const dalayPara = (index, nextWord) => {
+        setTimeout(function () {
+            setResultData(prev => prev + nextWord);
+        }, index * 75)
+    }
+
     const onSent = async (prompt) => {
         setResultData("")
         setLoading(true)
         setShowResult(true)
         setRecentPrompt(input) 
         const response = await run(input)
-        setResultData(response)
+        let responseArray = response.split("**")
+        let newResponse;
+        for (let i = 0; i < responseArray.length; i++) 
+        {
+            if (i === 0 || i % 2 !== 1 ) 
+            {
+                newResponse += responseArray[i];
+            }
+            else 
+            {
+                newResponse += "<b>" + responseArray[i] + "</b>";
+            }
+        }
+
+        let newResponse2 = newResponse.split("*").join("</br>")
+        let newResponseArray = newResponse2.split(" ");
+        for (let i = 0; i < newResponseArray.length; i++) 
+        {
+            const nextWord = newResponseArray[i];
+            dalayPara(i, nextWord + " ") 
+        }
+        
         setLoading(false)
         setInput("")
     }
